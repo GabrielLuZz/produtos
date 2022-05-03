48a550f0e34dbd567f7c6af7affc9f8ca670e447
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-05-02 22:22:41 -0300

    primeira tentativa de entrega

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,6 @@ import { App } from "./models/app.js";
 
 const { products } = await App.requisicao()
 
-console.log(products)
-
 products.forEach(product => {
     const productArea = App.template(product)
 
